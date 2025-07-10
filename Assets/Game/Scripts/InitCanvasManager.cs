82d58df0642e2c26f88851522dc60e98daf083dc
Andrei Retsikau <154879381+Retikoff@users.noreply.github.com>
2025-07-10 12:05:37 +0300

    InitCanvas

--- Assets/Game/Scripts/InitCanvasManager.cs
+++ Assets/Game/Scripts/InitCanvasManager.cs
@@ -9,14 +9,41 @@ public class InitCanvasManager : MonoBehaviour
     [SerializeField] private TMP_Dropdown categoryContainer;
     [SerializeField] private TMP_Text gameRuleTextContainer;
     [SerializeField] private Slider gameRuleSlider;
+    [SerializeField] private TMP_Text curGameRule;
+    [SerializeField] private Button wordAmountButton;
+    [SerializeField] private Button timeButton;
+    private GameRules gameRuleChoice = GameRules.Empty;
 
     private void Start()
     {
-        numTeamsSlider.onValueChanged.AddListener(delegate { UpdateCurrentNumTeams(); });   
+        numTeamsSlider.onValueChanged.AddListener(delegate { UpdateCurrentNumTeams(); });
+        gameRuleSlider.onValueChanged.AddListener(delegate { UpdateCurrentGameRule(); });
+        SwitchToWordAmount();
+    }
+
+    public void SwitchToWordAmount()
+    {
+        gameRuleChoice = GameRules.WordAmount;
+        gameRuleTextContainer.text = "Выберите кол.во слов на раунд";
+        wordAmountButton.GetComponentInChildren<TMP_Text>().color = Color.forestGreen;
+        timeButton.GetComponentInChildren<TMP_Text>().color = Color.black;
+    }
+
+    public void SwitchToTime()
+    {
+        gameRuleChoice = GameRules.Time;
+        gameRuleTextContainer.text = "Выберите время на раунд (мин)";
+        timeButton.GetComponentInChildren<TMP_Text>().color = Color.forestGreen;
+        wordAmountButton.GetComponentInChildren<TMP_Text>().color = Color.black;
     }
 
     public void UpdateCurrentNumTeams()
     {
         curNumTeams.text = numTeamsSlider.value.ToString();
-    } 
+    }
+
+    public void UpdateCurrentGameRule()
+    {
+        curGameRule.text = gameRuleSlider.value.ToString();
+    }
 }
